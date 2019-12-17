4009352ab8f52f4c1d5f40544b34ab2bd9564cdb
Maram Khattab <maram.khattab@hotmail.com>
2019-12-18 00:02:57 +0200

    routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 import Login from "./Containers/Login/Login";
@@ -8,7 +9,19 @@ import 'bootstrap/dist/css/bootstrap.css';
 function App() {
   return (
     <div className="App">
-      <Login />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+
+          <Route exact path="/signup" component={Signup} />
+          {/* <Route path="/" exact component={LandingPage} />
+            
+            <Route exact path="/profile/:screenName" component={Profile} />
+            
+            <Route exact path="/editprofile" component={EditProfile} /> */}
+
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
