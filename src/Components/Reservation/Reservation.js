75bba140f6fab8c05d204aaf20a1b735e8f78edb
Maram Khattab <maram.khattab@hotmail.com>
2019-12-25 01:25:58 +0200

    added cancel reservation

--- src/Components/Reservation/Reservation.js
+++ src/Components/Reservation/Reservation.js
@@ -15,9 +15,12 @@ class Reservation extends React.Component {
                     <div className="card-body" style={{ textAlign: "center" }}>
                         <p classNAme="card-text"><img style={{ margin: "0px", textAlign: "center", display: "inline-block" }} src="https://sunlimetech.com/portfolio/boot4menu/assets/imgs/team/img_01.png" /></p>
                         <h4 className="card-title"> {this.props.eventName} </h4>
-                        <p className="card-text"> {this.props.eventDate}</p>
-                        <p className="card-text">{this.props.eventHall}</p>
-                        <p className="card-text"> {this.props.eventTicketsCount}</p>
+                        <p className="card-text"> Date: {this.props.eventDate}</p>
+                        <p className="card-text"> Time: {this.props.eventTime}</p>
+                        <p className="card-text">Hall number:{this.props.eventHall}</p>
+                        <p className="card-text">Number of tickets: {this.props.eventTicketsCount}</p>
+                        <br />
+                        <p className="card-text"><button className="btn btn-danger"> Cancel reservation</button></p>
                     </div>
                 </div>
             </div >
